dcf6d548d0c3cc255755a181f3ecc768b39e4f73
Manuel Rodriguez <mrodriguezrodriguez@cifpfbmoll.eu>
2024-02-07 01:34:45 +0100

    a

--- Project/src/js/main.js
+++ Project/src/js/main.js
@@ -2,3 +2,94 @@ import '../bootstrap-5.3.2/scss/bootstrap.scss';
 import '../bootstrap-5.3.2/scss/bootstrap-grid.scss';
 import '../bootstrap-5.3.2/scss/bootstrap-utilities.scss';
 import '../bootstrap-5.3.2/scss/bootstrap-reboot.scss';
+
+function displayButtons() {
+    if (localStorage.getItem('userLog')) {
+    loginBoton.style.display = 'none';
+    registroBoton.style.display = 'none';
+    userinfo.style.display = 'block'
+    const userLog = JSON.parse(localStorage.getItem('userLog'));
+    userinfo.innerHTML = userLog[0].name;
+}
+else {
+    loginBoton.style.display = 'block'; 
+    registroBoton.style.display = 'block';
+    userinfo.style.display = 'none'
+}
+}
+
+
+// Definición de variables
+const noAccount = document.getElementById('nocuenta');
+const closeModal = document.getElementById('closeModal')
+const registroBoton = document.getElementById('registroBoton')
+const goLogin = document.getElementById('goLogin')
+const loginBoton = document.getElementById('loginBoton')
+const closeRegister = document.getElementById('closeRegister')
+const userinfo = document.getElementById('userinfo')
+
+
+noAccount.addEventListener('click', () => {
+    closeModal.click();
+    registroBoton.click();
+});
+goLogin.addEventListener('click', () => {
+    closeRegister.click()
+    loginBoton.click();
+});
+
+// Sistema de registro
+function saveDataRegister() {
+    let name = document.getElementById('nombre');
+    let email = document.getElementById('email');
+    let password = document.getElementById('password');
+    let level = document.getElementById('level')
+    let existingUsers = JSON.parse(localStorage.getItem('users')) || [];
+    existingUsers.push({
+        "name": name.value,
+        "email": email.value,
+        "password": password.value,
+        "level": level.value
+    });
+
+    localStorage.setItem('users', JSON.stringify(existingUsers));
+
+    if (closeRegister) {
+        closeRegister.click();
+    }
+}
+
+// Sistema login.
+function saveDataLogin() {
+let emailLogin = document.getElementById('emailLogin').value;
+let emailPassword = document.getElementById('emailPassword').value;
+let existingUsers = JSON.parse(localStorage.getItem('users'));
+existingUsers.forEach(user => {
+    if (user.email == emailLogin && user.password == emailPassword) {
+        let userLog = JSON.parse(localStorage.getItem('userLog')) || [];
+        userLog.push({
+            'name': user.name,
+            'email': user.email,
+            'password': user.password
+        });
+        localStorage.setItem('userLog', JSON.stringify(userLog));
+        userinfo.innerHTML = user.name;
+        displayButtons();
+    }
+
+    else {
+        alert('Not found')
+    }
+});
+}
+
+// Listeners
+let saveData = document.getElementById('saveData');
+saveData.addEventListener('click', saveDataLogin)
+let saveDataRegisterbutton = document.getElementById('saveDataRegister')
+saveDataRegisterbutton.addEventListener('click', saveDataRegister)
+let cerrarsesion = document.getElementById('cerrarsesion');
+cerrarsesion.addEventListener('click', () => {
+    localStorage.removeItem('userLog')
+    window.location.reload();
+})
