ca711d30fb55b790bf7ebb05eb7cd08f3e41293f
Manuel Rodriguez <mrodriguezrodriguez@cifpfbmoll.eu>
2024-02-07 19:07:02 +0100

    cerrar sesion bien

--- Project/src/js/profile.js
+++ Project/src/js/profile.js
@@ -1,11 +1,12 @@
 usuarioLog = localStorage.getItem('userLog');
-
 userJSON = JSON.parse(usuarioLog);
-
 let nombrePerfil = document.getElementById('nombrePerfil');
 let mailPerfil = document.getElementById('mailPerfil');
 let levelPerfil = document.getElementById('levelPerfil');
 
-nombrePerfil.innerText = userJSON[0].name;
-mailPerfil.innerText = userJSON[0].email;
-levelPerfil.innerText = userJSON[0].level;
+if (nombrePerfil, mailPerfil, levelPerfil) {
+    nombrePerfil.innerText = userJSON[0].name;
+    mailPerfil.innerText = userJSON[0].email;
+    levelPerfil.innerText = userJSON[0].level;
+}
+
