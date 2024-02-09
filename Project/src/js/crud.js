0b3a8d9abd48f0fda98ee62da801ae06e16e541e
Manuel Rodriguez <mrodriguezrodriguez@cifpfbmoll.eu>
2024-02-09 19:07:26 +0100

    a

--- Project/src/js/crud.js
+++ Project/src/js/crud.js
@@ -1,150 +1,145 @@
-const request = indexedDB.open("Reservas", 1);
-
 let db;
 
-request.onupgradeneeded = (event) => {
-  db = event.target.result;
-  console.log("BD creada", db);
-  const objectStore = db.createObjectStore("Reservas", {
-    keyPath: "id",
-    autoIncrement: true,
-  });
-  objectStore.createIndex("userIndex", "user", { unique: false });
-};
+function initDB() {
+  const request = indexedDB.open("Reservas", 1);
 
-request.onerror = (event) => {
-  console.log("Error", event);
-};
+  request.onupgradeneeded = (event) => {
+    db = event.target.result;
+    console.log("BD creada", db);
+    const objectStore = db.createObjectStore("Reservas", {
+      keyPath: "id",
+      autoIncrement: true,
+    });
+    objectStore.createIndex("userIndex", "user", { unique: false });
+  };
 
-request.onsuccess = (event) => {
-  db = event.target.result;
+  request.onerror = (event) => {
+    console.log("Error", event);
+  };
 
+  request.onsuccess = (event) => {
+    db = event.target.result;
+    addEventListeners();
+  };
+}
+
+function eliminarReserva(reservaId) {
+  const transaccion = db.transaction(["Reservas"], "readwrite");
+  const objectStore = transaccion.objectStore("Reservas");
+
+  const solicitud = objectStore.delete(reservaId);
+
+  solicitud.onsuccess = () => {
+    console.log("Reserva eliminada correctamente");
+    window.location.reload();
+  };
+
+  solicitud.onerror = () => {
+    console.error("Error al eliminar la reserva");
+  };
+}
+
+function addEventListeners() {
   const añadirBoton = document.getElementById("añadir");
-  const nombre = document.getElementById("nombre");
-  const centro = document.getElementById("centro");
-  const hora = document.getElementById("hora");
-  const date = document.getElementById("date");
+  const verTodo = document.getElementById("verTodo");
+  const byNameButton = document.getElementById("byName");
 
   if (añadirBoton) {
-    añadirBoton.addEventListener("click", () => {
-      const transaccion = db.transaction(["Reservas"], "readwrite");
-      const objectStore = transaccion.objectStore("Reservas");
-      usuarioLog = localStorage.getItem("userLog");
-      userJSON = JSON.parse(usuarioLog);
-      const nuevaReserva = {
-        name: nombre.value,
-        centro: {
-          name: centro.value,
-          lat: centro.options[centro.selectedIndex].getAttribute(
-            "data-latitud"
-          ),
-          lon: centro.options[centro.selectedIndex].getAttribute(
-            "data-longitud"
-          ),
-        },
-        hora: hora.value,
-        fecha: date.value,
-        user: userJSON[0].name,
-      };
-
-      const solicitud = objectStore.add(nuevaReserva);
-
-      solicitud.onsuccess = () => {
-        console.log("Reserva añadida correctamente");
-      };
-
-      solicitud.onerror = () => {
-        console.error("Error al añadir la reserva");
-      };
-    });
+    añadirBoton.addEventListener("click", añadirReserva);
   }
 
-  //MÍO
-  let verTodo = document.getElementById("verTodo");
   if (verTodo) {
-    verTodo.addEventListener("click", () => {
-      const transaccion = db.transaction(["Reservas"], "readonly");
-      const objectStore = transaccion.objectStore("Reservas");
-
-      const getAllRequest = objectStore.getAll();
-
-      getAllRequest.onsuccess = function (ev) {
-        const reservas = ev.target.result;
-        const tablaReservas = document.getElementById("tablaReservas");
-
-        // Limpiar la tabla antes de agregar nuevas filas
-        tablaReservas.innerHTML = "";
-
-        // Agregar una fila por cada reserva
-        reservas.forEach((reserva) => {
-          const fila = document.createElement("tr");
-          fila.innerHTML = `
-                    <td class="align-middle">${reserva.fecha}</td>
-                    <td class="align-middle">${reserva.hora}</td>
-                    <td class="align-middle">${reserva.centro.name}</td>
-                    <td class="align-middle">
-                        <button type="button" class="btn btn-success btn-sm" data-bs-toggle="modal" data-bs-target="#exampleModal">
-                        <i class="bi bi-gear-fill"></i>
-                        </button>
-                        <button type="button" class="btn btn-info btn-sm" data-bs-toggle="modal" data-bs-target="#exampleModal">
-                        <i class="bi bi-pencil-fill"></i>
-                        </button>
-                        <button type="button" class="btn btn-danger btn-sm" data-bs-toggle="modal" data-bs-target="#staticBackdrop">
-                        <i class="bi bi-calendar-x-fill"></i>
-                        </button>
-                    </td>`;
-
-          tablaReservas.appendChild(fila);
-        });
-      };
-
-      getAllRequest.onerror = function (ev) {
-        console.error("Error fetching data:", ev.target.error);
-      };
-    });
+    verTodo.addEventListener("click", verTodasLasReservas);
   }
-  
-  byNameButton = document.getElementById("byName");
+
   if (byNameButton) {
-    byNameButton.addEventListener("click", () => {
-      usuarioLog = localStorage.getItem("userLog");
-      userJSON = JSON.parse(usuarioLog);
-      const usuarioActual = userJSON[0].name;
-      const transaccion = db.transaction(["Reservas"], "readwrite");
-      const objectStore = transaccion.objectStore("Reservas");
-      const index = objectStore.index("userIndex");
-      const getAllRequest = objectStore.index('userIndex').getAll();
-      getAllRequest.onsuccess = (ev) => {
-        const cursor = ev.target.result;
-        console.log(cursor);
-        cursor.forEach(reserva => {
-            const fila = document.createElement("tr");
-            fila.innerHTML = `
-                      <td class="align-middle">${reserva.fecha}</td>
-                      <td class="align-middle">${reserva.hora}</td>
-                      <td class="align-middle">${reserva.centro.name}</td>
-                      <td class="align-middle">
-                        <button type="button" class="btn btn-success btn-sm" data-bs-toggle="modal" data-bs-target="#exampleModal">
-                        <i class="bi bi-gear-fill"></i>
-                        </button>
-                        <button type="button" class="btn btn-info btn-sm" data-bs-toggle="modal" data-bs-target="#exampleModal">
-                        <i class="bi bi-pencil-fill"></i>
-                        </button>
-                        <button type="button" class="btn btn-danger btn-sm" data-bs-toggle="modal" data-bs-target="#staticBackdrop">
-                        <i class="bi bi-calendar-x-fill"></i>
-                        </button>
-                    </td>`;
-  
-            tablaReservas.appendChild(fila);
-          });
-      };
-    });
+    byNameButton.addEventListener("click", verReservasPorNombre);
   }
-};
+}
 
-// MIO HASTA AQUÍ
-const ver = () => {};
+function añadirReserva() {
+  const transaccion = db.transaction(["Reservas"], "readwrite");
+  const objectStore = transaccion.objectStore("Reservas");
+  usuarioLog = localStorage.getItem("userLog");
+  userJSON = JSON.parse(usuarioLog);
+  const nuevaReserva = {
+    centro: {
+      name: centro.value,
+      lat: centro.options[centro.selectedIndex].getAttribute("data-latitud"),
+      lon: centro.options[centro.selectedIndex].getAttribute("data-longitud"),
+    },
+    hora: hora.value,
+    fecha: date.value,
+    user: userJSON[0].name,
+  };
 
-const actualizar = () => {};
+  const solicitud = objectStore.add(nuevaReserva);
 
-const eliminar = () => {};
+  solicitud.onsuccess = () => {
+    console.log("Reserva añadida correctamente");
+    let successReserva = document.getElementById('successReserva');
+    successReserva.innerText = 'Reserva añadida correctamente.'
+    successReserva.style.display = block;
+  };
+
+  solicitud.onerror = () => {
+    console.error("Error al añadir la reserva");
+  };
+}
+
+function verTodasLasReservas() {
+  const transaccion = db.transaction(["Reservas"], "readonly");
+  const objectStore = transaccion.objectStore("Reservas");
+  const getAllRequest = objectStore.getAll();
+
+  getAllRequest.onsuccess = function (ev) {
+    mostrarReservas(ev.target.result);
+  };
+
+  getAllRequest.onerror = function (ev) {
+    console.error("Error fetching data:", ev.target.error);
+  };
+}
+
+function verReservasPorNombre() {
+  const transaccion = db.transaction(["Reservas"], "readwrite");
+  const objectStore = transaccion.objectStore("Reservas");
+  const getAllRequest = objectStore.index('userIndex').getAll();
+
+  getAllRequest.onsuccess = (ev) => {
+    mostrarReservas(ev.target.result);
+  };
+}
+
+
+function mostrarReservas(reservas) {
+
+  const tablaReservas = document.getElementById("tablaReservas");
+  tablaReservas.innerHTML = "";
+
+  reservas.forEach((reserva) => {
+    const fila = document.createElement("tr");
+    fila.innerHTML = `
+      <td class="align-middle">${reserva.fecha}</td>
+      <td class="align-middle">${reserva.hora}</td>
+      <td class="align-middle">${reserva.centro.name}</td>
+      <td class="align-middle">
+        <button type="button" class="btn btn-success btn-sm" data-bs-toggle="modal" data-bs-target="#exampleModal">
+          <i class="bi bi-gear-fill"></i>
+        </button>
+        <button type="button" class="btn btn-info btn-sm" data-bs-toggle="modal" data-bs-target="#exampleModal">
+          <i class="bi bi-pencil-fill"></i>
+        </button>
+        <button id="deleteButton" type="button" class="btn btn-danger btn-sm" onclick="eliminarReserva(${reserva.id})">
+          <i class="bi bi-calendar-x-fill"></i>
+        </button>
+      </td>`;
+
+    tablaReservas.appendChild(fila);
+
+  });
+}
+
+
+// Inicializar la base de datos al cargar la página
+document.addEventListener("DOMContentLoaded", initDB);
